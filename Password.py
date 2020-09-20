a1874ce3918146e0d85d7588a5427ba6931418b5
Gaushik M.R <gaushik126@gmail.com>
2020-09-20 12:10:42 -0400

    Update Password.py

--- Password.py
+++ Password.py
@@ -1,1 +1,42 @@
 
+
+
+
+
+########################################################
+#        Program to check password strength            #
+#                                                      #
+#                                                      #
+#                 Author - Gaushik MR                 #
+#                                                      #
+#                  dated- 16 July 2020                 #
+#                				       #
+########################################################
+
+
+import re
+p = raw_input("Enter a password:  ")
+
+x = True
+while x:  
+    if (len(p)<6 or len(p)>12):
+        break
+    elif not p[0].isalpha():
+        break
+    elif not re.search("[a-z]",p):
+        break
+    
+    elif not re.search("[A-Z]",p):
+        break
+    elif not re.search("[$#@]",p):
+        break
+    elif re.search("\s",p):
+        break
+
+    else:
+        print("Strong Password")
+        x=False
+        break
+
+if x:
+    print("Weak Password")
